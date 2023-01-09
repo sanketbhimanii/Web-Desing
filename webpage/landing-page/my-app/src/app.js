f52eede03b70c9661f0367cfbd05cb25b82b0546
sanketbhimanii <114909859+sanketbhimanii@users.noreply.github.com>
2023-01-09 10:30:36 +0530

    Revert "create Sing in page"

---
+++ webpage/landing-page/my-app/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../src/app.css';
+import Header from './Header';
+import { Button } from '@mui/material';
+
+const App = () => {
+  return (
+     <div className='classes-root'>
+     
+     <Header/>
+    </div>
+     
+  )
+}
+
+export default App
--- webpage/landing-page/my-app/src/index.js
+++ webpage/landing-page/my-app/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Island from './iselandLanding-page/Island';
-import SignIn from './SignIn-page/SignIn';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    {/* <Island /> */}
-    <SignIn/>
+    <App />
   </>
 );
 
